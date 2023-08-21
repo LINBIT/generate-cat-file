9689981a90559a625b5552fee536aa1a410abd8e
Johannes Thoma <johannes@johannesthoma.com>
2023-08-21 15:04:58 +0000

    write parameter now in stack.
    Allows for sequences inside sequences.

--- generate-cat-file.c
+++ generate-cat-file.c
@@ -37,7 +37,6 @@ struct pkcs7_toplevel {
 	int y;
 };
 
-bool write;
 size_t buflen;
 char buffer[1024*1024];
 
@@ -47,7 +46,7 @@ void __attribute((noreturn)) fatal(const char *msg)
 	exit(1);
 }
 
-size_t append_to_buffer(size_t n, char *data)
+size_t append_to_buffer(size_t n, char *data, bool write)
 {
 	if (buflen + n >= sizeof(buffer)) {
 		fatal("buffer size too small, please recompile with bigger buffer.\n");
@@ -60,7 +59,7 @@ size_t append_to_buffer(size_t n, char *data)
 	return n;
 }
 
-size_t encode_integer(int i)
+size_t encode_integer(int i, bool write)
 {
 	char int_buf[10] = { INTEGER_TAG, };
 	char sign = 0;
@@ -72,20 +71,20 @@ size_t encode_integer(int i)
 	if (i < 0x80) {
 		int_buf[1] = 3;	/* length */
 		int_buf[2] = i | sign;
-		return append_to_buffer(3, int_buf);
+		return append_to_buffer(3, int_buf, write);
 	}
 	if (i < 0x8000) {
 		int_buf[1] = 4;	/* length */
 		int_buf[2] = (i >> 8) | sign;
 		int_buf[3] = i & 0xff;
-		return append_to_buffer(4, int_buf);
+		return append_to_buffer(4, int_buf, write);
 	}
 	if (i < 0x800000) {
 		int_buf[1] = 4;	/* length */
 		int_buf[2] = (i >> 16) | sign;
 		int_buf[3] = (i >> 8) & 0xff;
 		int_buf[4] = i & 0xff;
-		return append_to_buffer(5, int_buf);
+		return append_to_buffer(5, int_buf, write);
 	}
 	if (i < 0x80000000) {
 		int_buf[1] = 5;	/* length */
@@ -93,62 +92,59 @@ size_t encode_integer(int i)
 		int_buf[3] = (i >> 16) & 0xff;
 		int_buf[4] = (i >> 8) & 0xff;
 		int_buf[5] = i & 0xff;
-		return append_to_buffer(6, int_buf);
+		return append_to_buffer(6, int_buf, write);
 	}
 	fatal("Can't encode this integer\n");
 }
 
-size_t encode_null(void)
+size_t encode_null(bool write)
 {
 	char null_buf[2] = { NULL_TAG, 0x00 };
-	return append_to_buffer(sizeof(null_buf), null_buf);
+	return append_to_buffer(sizeof(null_buf), null_buf, write);
 }
 
-size_t encode_tag_and_length(char tag, size_t length)
+size_t encode_tag_and_length(char tag, size_t length, bool write)
 {
 	char tag_and_length[10];
 	tag_and_length[0] = tag;
 
 	if (length < 0x80) {
 		tag_and_length[1] = length;
-		return append_to_buffer(2, tag_and_length);
+		return append_to_buffer(2, tag_and_length, write);
 	}
 	if (length < 0x10000) {
 		tag_and_length[1] = 0x82;	/* 2 more length bytes */
 		tag_and_length[2] = (length >> 8) & 0xff;
 		tag_and_length[3] = length & 0xff;
-		return append_to_buffer(4, tag_and_length);
+		return append_to_buffer(4, tag_and_length, write);
 	}
 	if (length < 0x1000000) {
 		tag_and_length[1] = 0x83;	/* 3 more length bytes */
 		tag_and_length[2] = (length >> 16) & 0xff;
 		tag_and_length[3] = (length >> 8) & 0xff;
 		tag_and_length[4] = length & 0xff;
-		return append_to_buffer(5, tag_and_length);
+		return append_to_buffer(5, tag_and_length, write);
 	}
 	fatal("This length is not supported");
 }
 
-size_t encode_pkcs7_toplevel(void *p)
+size_t encode_pkcs7_toplevel(void *p, bool write)
 {
 	struct pkcs7_toplevel *s = p;
 	size_t length = 0;
 
-	length += encode_integer(s->x);
-	length += encode_integer(s->y);
+	length += encode_integer(s->x, write);
+	length += encode_integer(s->y, write);
 
 	return length;
 }
 
-size_t encode_sequence(void *s, size_t a_fn(void*))
+size_t encode_sequence(void *s, size_t a_fn(void*, bool), bool write)
 {
-	write = false;
-	size_t length = a_fn(s);
+	size_t length = a_fn(s, false);
 
-	write = true;
-	size_t l2 = encode_tag_and_length(SEQUENCE_TAG, length);
-
-	return a_fn(s) + l2;
+	size_t l2 = encode_tag_and_length(SEQUENCE_TAG, length, write);
+	return a_fn(s, write) + l2;
 }
 
 int main(int argc, char ** argv)
@@ -159,7 +155,7 @@ int main(int argc, char ** argv)
 
 	/* compute lengths */
 	/* generate binary DER */
-	len = encode_sequence(&s, encode_pkcs7_toplevel);
+	len = encode_sequence(&s, encode_pkcs7_toplevel, true);
 	if (len != buflen)
 		fatal("length mismatch\n");
 
