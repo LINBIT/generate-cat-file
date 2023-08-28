ac173a1b4c29d8ce97cd461e9cbd243d7a32a467
Johannes Thoma <johannes@johannesthoma.com>
2023-08-28 12:12:29 +0000

    gencat.sh: working on getopt(1)

--- gencat.sh
+++ gencat.sh
@@ -3,10 +3,6 @@
 # from catgen:
 #  -o, --out
 #        output cat file
-#  -d, --drv-path
-#        dir containing files
-#  -i, --inf-file
-#        parse inf file
 #  -h, --hwid
 #        hwid (example: PNP0F13)
 #  -O, --OS
@@ -14,9 +10,44 @@
 #  -A, --OSAttr
 #        OSAttr string (default: 2:6.1,2:6.2,2:6.4)
 
+OUTPUT_CAT_FILE=-
+HARDWARE_ID=windrbd
+OS_STRING=7X64,8X64,10X64
+OS_ATTR=2:6.1,2:6.2,2:6.4
+
+args=$( getopt o:h:O:A: $* )
+if [ $? -ne 0 ]
+then
+echo $#
+	echo Usage: "$0 -o <output-file> [-h <hardware-id>] [-O OS string] [-A OS attribute string] file1 [ file2 ... ]"
+	exit 1
+fi
+
+set -- $args
+
+for arg
+do
+	case "$arg"
+	in
+		-o)
+			OUTPUT_CAT_FILE=$2
+			shift
+			shift
+			;;
+		--)
+			shift
+			break
+			;;
+	esac
+done
+
+echo "$# arguments remaining"
+echo " -o is $OUTPUT_CAT_FILE"
+
 i=0
 for f in $*
 do
+	echo File is $f ...
 	if ./strip-pe-image $f > /dev/null 2>/dev/null
 	then
 		images[$i]=$( basename $f ):$( ./strip-pe-image $f | sha1sum | cut -d' ' -f 1 ):PE
